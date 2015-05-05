e435ea11a5b3f229d7260a7abc0077a6cea2acbc
xeli <akseli.palen@gmail.com>
2015-05-05 16:00:37 +0300

    Test for GazeApiConnection.connect.

--- test/GazeApiConnection.test.js
+++ test/GazeApiConnection.test.js
@@ -5,17 +5,42 @@ var GazeApiConnection = require('../lib/GazeApiConnection');
 
 describe('GazeApiConnection', function () {
 
-  var gazecon;
+  var gazecon, servermock;
 
-  beforeEach(function () {
-    gazecon = new GazeApiConnection();
+  beforeEach(function (done) {
+    servermock = new ServerMock(6555, function () {
+      done();
+    });
+  });
+
+  afterEach(function (done) {
+    servermock.close(function (err) {
+      if (err) { console.error(err); throw err; }
+      done();
+    });
   });
 
   describe('#connect()', function () {
+
     it('should be a function', function () {
+      gazecon = new GazeApiConnection();
       gazecon.should.have.property('connect');
       (gazecon.connect).should.be.a.Function;
     });
+
+    it('should establish a connection with the server', function (done) {
+      servermock.getNumConnections(function (numConnections) {
+        numConnections.should.equal(0);
+        gazecon.connect('localhost', 6555, function (isConnected) {
+          isConnected.should.be.True;
+          servermock.getNumConnections(function (numConnections) {
+            numConnections.should.equal(1);
+            done();
+          });
+        });
+      });
+    });
+
   });
 
 });
--- test/eyetribe-server-mock.js
+++ test/eyetribe-server-mock.js
@@ -3,15 +3,20 @@
  */
 var net = require('net');
 
-var ServerMock = function (port) {
+var ServerMock = function (port, onListening) {
   var server, host;
 
   // Defaults
-  if (typeof port !== 'number') { port = 6555; }
-  host = 'localhost';
+  var DEFAULT_HOST = 'localhost';
+  var DEFAULT_PORT = 6555;
+  var NOOP = function () {};
+  if (typeof port === 'function') { onListening = port; port = DEFAULT_PORT; }
+  if (typeof port !== 'number') { port = DEFAULT_PORT; }
+  if (typeof onListening !== 'function') { onListening = NOOP; }
+  host = DEFAULT_HOST;
 
   server = net.createServer(function (socket) {
-    socket.end('Hello and bye');
+    // socket.end('Hello and bye');
   });
 
   server.on('error', function (err) {
@@ -26,9 +31,21 @@ var ServerMock = function (port) {
     }
   });
 
-  server.listen(port, function onListening() {
+  server.listen(port, function () {
+    onListening();
     console.log('The Eye Tribe Tracker Server listening port ' + port);
   });
+
+  this.getNumConnections = function (callback) {
+    server.getConnections(function (err, num) {
+      if (err) { console.error(err); callback(null); return; }
+      callback(num);
+    });
+  };
+
+  this.close = function (onClosed) {
+    server.close(onClosed);
+  };
 };
 
 module.exports = ServerMock;
